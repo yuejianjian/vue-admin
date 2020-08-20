8202607a237fbb50e18a24facc0441a3aa4cdd21
yuejianjian <yuejianjian@kuaixiaohulian.com>
2020-08-20 14:27:47 +0800

    第四次提交

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -7,7 +7,7 @@
         <el-button type="success">成功按钮</el-button>
         <el-button type="info">信息按钮</el-button>
         <el-button type="warning">警告按钮</el-button>
-        <el-button type="danger">危险按钮1</el-button>
+        <el-button type="danger">危险按1钮1</el-button>
     </el-row>
   </div>
 </template>
