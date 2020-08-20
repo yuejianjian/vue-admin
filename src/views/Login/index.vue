e8d12df6c3ee79f8ce1fe7ef9d4d6b006c3d21d1
yuejianjian <yuejianjian@kuaixiaohulian.com>
2020-08-20 14:31:42 +0800

    第五次提交

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -6,7 +6,7 @@
         <el-button type="primary">主要按钮</el-button>
         <el-button type="success">成功按钮</el-button>
         <el-button type="info">信息按钮</el-button>
-        <el-button type="warning">警告按钮</el-button>
+        <el-button type="warning">警告11按钮</el-button>
         <el-button type="danger">危险按1钮1</el-button>
     </el-row>
   </div>
